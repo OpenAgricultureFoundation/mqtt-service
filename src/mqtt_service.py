40efa9803083f9d6585c87767d623a1667a80a37
Stephen Moore <srmoore@gmail.com>
2020-06-02 15:03:02 -0400

    Update to use local backend mqtt_service with influxDB configuration

--- src/mqtt_service.py
+++ src/mqtt_service.py
@@ -16,9 +16,12 @@ from cloud_common.cc.mqtt.local_mqtt_messaging import MQTTMessaging
 
 
 #------------------------------------------------------------------------------
+# This is for old Google based pubsub
 # Sadly, we have to use a global class instance here, since the pubsub message
 # callback signature won't let me call a class method.
-mqtt_messaging = MQTTMessaging()
+# mqtt_messaging = MQTTMessaging()
+
+mqtt_messaging = None # this will be setup in __main__()
 
 
 #------------------------------------------------------------------------------
@@ -33,7 +36,7 @@ signal.signal( signal.SIGINT, signal_handler )
 # This callback is called for each message we receive on the device events 
 # pubsub topic.
 # We acknowledge the message, then validate and act on it if valid.
-def callback(mqttc, obj, msg):
+def callback(mqttc, userdata, msg):
     try:
         #msg.ack() # acknowledge to the server that we got the message
         logging.error("RECIVED MESSAGE")
@@ -57,7 +60,10 @@ def callback(mqttc, obj, msg):
 
         # finally let our mqtt class parse this message and decide how to 
         # handle it
-        mqtt_messaging.parse(deviceId, pydict)
+        if userdata['mqtt_messaging'] is not None:
+            userdata['mqtt_messaging'].parse(deviceId, pydict)
+        else:
+            logging.error('No mqtt_messaging defined while in callback')
 
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -85,6 +91,15 @@ def main():
     parser.add_argument('--name', type=str,
                         help='Set the mqtt client name, defaults to openag_mqtt_service',
                         default='openag_mqtt_service')
+    parser.add_argument('--db_host', type=str,
+                        help='Set a local influxdb host, default `influxdb`',
+                        default='influxdb')
+    parser.add_argument('--db_port', type=int,
+                        help='Set a local influxdb port, default 8086',
+                        default=8086)
+    parser.add_argument('--db_name', type=str,
+                        help='Set the influx DB name, defaults to openag_local',
+                        default='openag_local')
     args = parser.parse_args()
 
     # User specified log level.
@@ -98,6 +113,11 @@ def main():
     mqtt_host = args.host
     mqtt_port = args.port
 
+    db_host = args.db_host
+    db_port = args.db_port
+    db_name = args.db_name
+    client_userdata = {'mqtt_messaging': MQTTMessaging(host=db_host, port=db_port, db_name=db_name)}
+
     # Make sure our env. vars are set up.
     #if None == env_vars.cloud_project_id or None == env_vars.dev_events or \
     #        None == env_vars.bq_dataset or None == env_vars.bq_table or \
@@ -110,7 +130,7 @@ def main():
     # Infinetly re-subscribe to this topic and receive callbacks for each
     # message.
     # pubsub.subscribe(env_vars.cloud_project_id, env_vars.dev_events, callback)
-    client = mqtt.Client(args.name)
+    client = mqtt.Client(args.name, userdata=client_userdata)
     client.on_message = callback
     client.connect(host=mqtt_host, port=mqtt_port)
 
